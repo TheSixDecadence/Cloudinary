547f80cdbf8891f517b71aa4a0ee95df291959d5
alvaradodaniel <alvaradodaniel128@gmail.com>
2024-06-14 09:12:24 -0600

    :lipstick:Adding gallery

--- pages/CameraPage.tsx
+++ pages/CameraPage.tsx
@@ -1,38 +1,86 @@
-import { CameraView, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useEffect, useRef, useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import * as ImagePicker from 'expo-image-picker';
 
 export default function CameraPage() {
-  const [facing, setFacing] = useState('back');
-  const [permission, requestPermission] = useCameraPermissions();
+  const [facing, setFacing] = useState("back");
+  const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+  const cameraRef = useRef(null as any);
+  const [mediaPermission, requestMediaPermission] =
+    MediaLibrary.usePermissions();
+  const [image, setImage] = useState(null);
 
-  if (!permission) {
-    // Camera permissions are still loading.
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  if (!cameraPermission && !mediaPermission) {
     return <View />;
   }
 
-  if (!permission.granted) {
+  if (!cameraPermission?.granted && !mediaPermission?.granted) {
     // Camera permissions are not granted yet.
     return (
       <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button
+          onPress={() => {
+            requestCameraPermission();
+            requestMediaPermission();
+          }}
+          title="grant permission"
+        />
       </View>
     );
   }
 
   function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+  function takePicture() {
+    if (!mediaPermission?.granted && mediaPermission?.canAskAgain) {
+      requestMediaPermission();
+    }
+    if (cameraRef.current) {
+      cameraRef.current
+        .takePictureAsync({ skipProcessing: true })
+        .then(async (picture: any) => {
+          try {
+            await MediaLibrary.saveToLibraryAsync(picture.uri);
+          } catch (error) {
+            alert("We couldn't take the picture");
+          }
+        });
+    }
   }
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-      <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={()=>alert("GALERIA")}>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={pickImage}> 
             <Text style={styles.text}>Gallery</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.takePictureButton} onPress={()=>alert("PHOTO")}>
+          <TouchableOpacity
+            style={styles.takePictureButton}
+            onPress={takePicture}
+          >
             <Text style={styles.text}>Take Photo</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
@@ -44,39 +92,41 @@ export default function CameraPage() {
   );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-    camera: {
-      flex: 1,
-    },
-    buttonContainer: {
-      flex: 1,
-      flexDirection: 'row',
-      backgroundColor: 'transparent',
-      margin: 64,
-    },
-    button: {
-      flex: 1,
-      alignSelf: 'flex-end',
-      alignItems: 'center',
-    },
-    text: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      color: 'white',
-    },
-    takePictureButton: {
-        width: 70,
-        height: 70,
-        borderRadius: 35,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        bottom: 30,
-        alignSelf: 'center',
-        flex: .5,
-      },
-  });
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+  takePictureButton: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 50,
+    left: "50%",
+    marginLeft: -35,
+    alignSelf: "center",
+    flex: 1,
+  },
+});
